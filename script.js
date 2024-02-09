ec66ae138891c83418e649b2150ba307c53b9924
Fedorov Mitriy <58570733+Fanzholl@users.noreply.github.com>
2024-02-10 01:49:50 +0300

    A patch with the addition of a computer opponent.
    A patch with the addition of a computer opponent. Good luck!

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const gameMatrix = [
     [0, 0, 0],
     [0, 0, 0],
@@ -12,24 +14,48 @@ const winner = document.querySelector(`h1`);
 let steps = 1;
 let winS = null;
 
+let pcStep = new CustomEvent('pcstep', {
+    bubbles: true,
+})
+
 table.addEventListener('click', matrixEdit);
+table.addEventListener('pcstep', matrixEdit);
 
 function matrixEdit (event) {
-    const target = event.target;
+    if (event) {
+        if (event.target.tagName == 'P' || event.target.children[0].textContent != '') return;
+    }
 
-    if (event.target.tagName == 'P' || event.target.children[0].textContent != '') return;
+    let step = null
 
-    const row = Array.prototype.indexOf.call(trCollection, target.closest(`tr`));
-    const rowElement = Array.prototype.indexOf.call(target.closest(`tr`).querySelectorAll(`td`), target);
+    if (steps % 2 !== 0) {
 
-    const step = gameMatrix[row][rowElement] = steps % 2 === 0 ? target.children[0].textContent = 'O' : target.children[0].textContent = 'X';
+        let row = Array.prototype.indexOf.call(trCollection, event.target.closest(`tr`));
+        let rowElement = Array.prototype.indexOf.call(event.target.closest(`tr`).querySelectorAll(`td`), event.target);
+        step = gameMatrix[row][rowElement] = steps % 2 === 0 ? target.children[0].textContent = 'O' : event.target.children[0].textContent = 'X';
+
+    } else if (steps % 2 === 0) {
+        let rowElement = null;
+        const rowMath = Math.floor(Math.random() * 3);
+        const rowElementMath = Math.floor(Math.random() * 3);
+        pcBot(trCollection, gameMatrix);
+        step = 'O';
+    }
 
     ++steps;
+    
 
     if (compareWin(gameMatrix, step) || steps >= 10) {
         winner.textContent = compareWin(gameMatrix, step) || `All loose`;
         table.removeEventListener('click', matrixEdit);
+        return;
     }
+
+    if (steps % 2 == 0) {
+        matrixEdit();
+    }
+
+    
 }
 
 
@@ -66,7 +92,7 @@ function compareWin(gameMatrix, step) {
 
     if (win < 3) {
         let d = 0;
-        for (i = 3 - 1; i >= 0; i--) {
+        for (let i = 3 - 1; i >= 0; i--) {
             win += gameMatrix[d][i] == step; ++d;
         }
     }
@@ -74,4 +100,16 @@ function compareWin(gameMatrix, step) {
     if (win == 3) return `${step} is win!`;
 
     return null;
+}
+
+function pcBot(trCollection, gameMatrix) {
+    const rowMath = Math.floor(Math.random() * 3);
+    const rowElementMath = Math.floor(Math.random() * 3);
+    const row = trCollection[rowMath];
+    const rowElement = row.children[rowElementMath];
+    if (rowElement.children[0].textContent !== '') {
+        pcBot(trCollection, gameMatrix)
+    } else {
+        gameMatrix[rowMath][rowElementMath] = rowElement.children[0].textContent = 'O';
+    }
 }
